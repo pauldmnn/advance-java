c8f29620c7e33da1454db4a16c822d91b9d91456
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-09-05 07:34:04 +0000

    Reduce method lesson

---
+++ reduce.js
@@ -0,0 +1,51 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+
+const nums = [2, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr);
+console.log(sum)
+
+// break down of the above function
+const nums1 = [2, 2, 3, 4, 5];
+let sum1 = nums1.reduce((acc, curr) => {
+    console.log(
+      "Accumulator", acc, 
+      "Current Value", curr,
+      "Total", acc + curr)
+    return acc + curr
+});
+console.log(sum1)
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  
+  
+  // Grouping by a property, and totaling it too
