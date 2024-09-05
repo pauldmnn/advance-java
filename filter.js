1e4a37695a5d189e8080e71299ba78d320334a4b
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-09-05 06:47:34 +0000

    Filter lesson add more information

--- filter.js
+++ filter.js
@@ -20,7 +20,14 @@ const people = [
       age: 18,
     },
   ];
+
+  // to filter people aged 21 or over
+  const oldEnough = people.filter(person => person.age >= 21);
+  console.log(oldEnough);
   
+  // to filter a name
+  const filterName = people.filter(person => person.name === "Paul");
+  console.log(filterName)
   
   // Complex Filtering
   const students = [
@@ -55,3 +62,36 @@ const people = [
       ]
     },
   ];
+
+       // OPTION 1
+
+       //filter student with at least 5 years experience in at least one skill
+       // can be done this way bu it is hard to understand 
+
+// const candidates = students.filter(student => {
+//     let strongSkills = student.skills.filter(skill => skill.yrsExperience >=5);
+//     return strongSkills.lenght > 0
+// })
+// console.log(candidates)
+
+
+
+        // OPTION 2
+
+// const hasStrongSkills = student => {
+//     let strongSkills = student.skills.filter(skill => skill.yrsExperience >=5);
+//     return strongSkills.lenght > 0;
+// };
+// const candidates = students.filter(hasStrongSkills)
+// console.log(candidates)
+
+
+      //OPTION 3
+
+
+const has5yearsExp = skill => skill.yrsExperience >=5
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).lenght > 0;
+const candidates = students.filter(hasStrongSkills)
+console.log(candidates)
+
+// It is easyer to read if you pass the callback function first and then pass it into the filter section 
